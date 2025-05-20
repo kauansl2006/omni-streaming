ba841bb02c850d8257fea673f4dc37822e76e5ec
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-20 08:53:40 -0300

    chore: define fonte e metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,21 @@
+import { Manrope } from "next/font/google";
 import "./globals.css";
+
+const manrope = Manrope({
+  weight: ["400", "500", "600", "700"],
+  style: "normal",
+  variable: "--font-manrope",
+});
+
+export const metadata = {
+  title: "omni - get streaming now",
+  description: "The best plataform to find movies and TV series.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
