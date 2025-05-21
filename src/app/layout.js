effe418dbe8ee2213ecbbe9d0a05e22288c1248e
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-21 15:25:50 -0300

    feat: adiciona nova estrutura para RootLayout e HomePage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Manrope } from "next/font/google";
+
 import "./globals.css";
+
+import { HeaderComponent } from "./_components/header/header";
+import { FooterComponent } from "./_components/footer/footer";
 
 const manrope = Manrope({
   weight: ["400", "500", "600", "700"],
@@ -15,7 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={manrope.className}>{children}</body>
+      <body className={manrope.className}>
+        <HeaderComponent />
+        {children}
+        <FooterComponent />
+      </body>
     </html>
   );
 }
