91d56349123d319219b62a444d8ee1c53fadb2bd
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-23 11:14:10 -0300

    refactor: atualiza estilos da MoviesAndShowsPage

--- src/app/(pages)/movies-&-shows/page.js
+++ src/app/(pages)/movies-&-shows/page.js
@@ -1,54 +1,38 @@
 import styles from "./page.module.css";
-import MoviesListComponent from "@/app/_components/movies-list/movies-list";
-import ShowsListComponent from "@/app/_components/shows-list/shows-list";
+
 import { MOVIES } from "@/app/_dataset/movies";
 import { SHOWS } from "@/app/_dataset/shows";
 
-export default async function MoviesAndShowsPage() {
+import ShowcaseComponent from "@/app/_components/showcase/showcase";
+import CardsListComponent from "@/app/_components/cards-list/cards-list";
+
+export default function MoviesAndShowsPage() {
   return (
-    <main className={styles["main__mands"]}>
-      <section className={styles["section__mands"]}>
-        <div className={styles["container__mands"]}>
-          <div className={styles["subcontainer__mands"]}>
-            <div className={styles["text-container__mands"]}>
-              <h6 className={styles["heading__mands"]}>Movies</h6>
-            </div>
+    <main className={styles["main__movies-and-shows"]}>
+      <section className={styles["section__movies-and-shows"]}>
+        <div className={styles["container__movies-and-shows"]}>
+          <div className={styles["text-container__movies-and-shows"]}>
+            <h6 className={styles["h6__movies-and-shows"]}>Movies</h6>
           </div>
 
-          <div className={styles["subcontainer__mands"]}>
-            <div className={styles["container__mands"]}>
-              <div className={styles["subcontainer-movies__mands"]}>
-                <h2 className={styles["heading__mands"]}>Discover Movies</h2>
-                <div className={styles["buttons-container__mands"]}>
-                  <button className={styles["button__mands"]}>{" < "}</button>
-                  <button className={styles["button__mands"]}>{" > "}</button>
-                </div>
-              </div>
-              <MoviesListComponent movies={MOVIES} />
-            </div>
+          <div className={styles["showcase-container__movies-and-shows"]}>
+            <ShowcaseComponent heading={"Discover Movies"}>
+              <CardsListComponent items={MOVIES} />
+            </ShowcaseComponent>
           </div>
         </div>
       </section>
 
-      <section className={styles["section__mands"]}>
-        <div className={styles["container__mands"]}>
-          <div className={styles["subcontainer__mands"]}>
-            <div className={styles["text-container__mands"]}>
-              <h6 className={styles["heading__mands"]}>Shows</h6>
-            </div>
+      <section className={styles["section__movies-and-shows"]}>
+        <div className={styles["container__movies-and-shows"]}>
+          <div className={styles["text-container__movies-and-shows"]}>
+            <h6 className={styles["h6__movies-and-shows"]}>TV Shows</h6>
           </div>
 
-          <div className={styles["subcontainer__mands"]}>
-            <div className={styles["container__mands"]}>
-              <div className={styles["subcontainer-shows__mands"]}>
-                <h2 className={styles["heading__mands"]}>TV Shows</h2>
-                <div className={styles["buttons-container__mands"]}>
-                  <button className={styles["button__mands"]}>{" < "}</button>
-                  <button className={styles["button__mands"]}>{" > "}</button>
-                </div>
-              </div>
-              <ShowsListComponent shows={SHOWS} />
-            </div>
+          <div className={styles["showcase-container__movies-and-shows"]}>
+            <ShowcaseComponent heading={"Discover TV Shows"}>
+              <CardsListComponent items={SHOWS} />
+            </ShowcaseComponent>
           </div>
         </div>
       </section>
