73d13d390a3954dda2b058143b9de2bbff2b149e
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-23 11:15:12 -0300

    feat: cria componente Showcase

---
+++ src/app/_components/showcase/showcase.js
@@ -0,0 +1,19 @@
+import styles from "./showcase.module.css";
+
+export default function ShowcaseComponent({ heading, children }) {
+  return (
+    <section className={styles["section__showcase"]}>
+      <div className={styles["container__showcase"]}>
+        <div className={styles["heading-container__showcase"]}>
+          <h2 className={styles["h2__showcase"]}>{heading}</h2>
+          <div className={styles["buttons__showcase"]}>
+            <button className={styles["button__showcase"]}>{"<"}</button>
+            <button className={styles["button__showcase"]}>{">"}</button>
+          </div>
+        </div>
+
+        <div className={styles["cards-container__showcase"]}>{children}</div>
+      </div>
+    </section>
+  );
+}
