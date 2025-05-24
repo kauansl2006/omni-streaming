d6e492d9526144b21e8d4244ed7a6e9d68e7d49f
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-24 14:51:51 -0300

    feat: criar componente SubscriptionCard

---
+++ src/components/SubscriptionSection/SubscriptionCard/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./subscription-card.module.css"
+
+export const SubscriptionCardComponent = ({ item }) => {
+  return (
+    <div className={styles["card__subscription"]}>
+      <div className={styles["text-container__subscription"]}>
+        <h4 className={styles["heading__subscription"]}>{item.title}</h4>
+        <p className={styles["p__subscription"]}>{item.description}</p>
+      </div>
+
+      <div className={styles["price-container__subscription"]}>
+        <h3 className={styles["price__subcription"]}>{item.price}</h3>
+        <p className={styles["price-text__subscription"]}>{`/${item.type}`}</p>
+      </div>
+
+      <div className={styles["buttons-container"]}>
+        <button className={styles["button__subscription"]}>Start Free Trial</button>
+        <button className={styles["button__subscription"]}>Choose Plan</button>
+      </div>
+    </div>
+  )
+}
