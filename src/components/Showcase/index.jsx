494f0a5d9af12e01eb2dae04bb61c4be9badb3d9
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-24 09:02:47 -0300

    feat: adiciona condicionais para renderizar os cards

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -5,37 +5,48 @@ import { Suspense, useRef } from "react";
 import styles from "./showcase.module.css";
 
 import { CardsListComponent } from "../CardsList";
+import { CategoriesListComponent } from "../CategoriesList";
 
-export const ShowcaseComponent = ({ heading, items }) => {
+export const ShowcaseComponent = ({ heading, items, type }) => {
   const swiperRef = useRef(null)
 
   return (
     <section className={styles["section__showcase"]}>
       <div className={styles["container__showcase"]}>
-          <div className={styles["heading-container__showcase"]}>
-            <h2 className={styles["h2__showcase"]}>{heading}</h2>
+        <div className={styles["heading-container__showcase"]}>
+          <h2 className={styles["h2__showcase"]}>{heading}</h2>
+          {
+            !(type === "categories") && (
 
-            <div className={styles["buttons__showcase"]}>
-              <button
-                className={styles["button__showcase"]}
-                onClick={() => swiperRef.current?.slidePrev()}
-              >
-                {"<"}
-              </button>
-              <button
-                className={styles["button__showcase"]}
-                onClick={() => swiperRef.current?.slideNext()}
-              >
-                {">"}
-              </button>
-            </div>
-          </div>
+              <div className={styles["buttons__showcase"]}>
+                <button
+                  className={styles["button__showcase"]}
+                  onClick={() => swiperRef.current?.slidePrev()}
+                >
+                  {"<"}
+                </button>
+                <button
+                  className={styles["button__showcase"]}
+                  onClick={() => swiperRef.current?.slideNext()}
+                >
+                  {">"}
+                </button>
+              </div>
+            )
+          }
+        </div>
 
-          <div className={styles["cards-container__showcase"]}>
-            <Suspense fallback={<div className={styles["loading__showcase"]}>Loading...</div>}>
-              <CardsListComponent items={items} swiperRef={swiperRef} />
-            </Suspense>
-          </div>
+        <div className={styles["cards-container__showcase"]}>
+          <Suspense fallback={<div className={styles["loading__showcase"]}>Loading...</div>}>
+            {
+              type === "categories" ? (
+                <CategoriesListComponent items={items} swiperRef={swiperRef} />
+              ) : (
+                <CardsListComponent items={items} swiperRef={swiperRef} />
+              )
+            }
+          </Suspense>
+        </div>
       </div>
     </section>
   );
