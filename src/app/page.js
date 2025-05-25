d9274a5536c036f965b0bd256a95d54db762083a
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-25 11:25:53 -0300

    refactor: atualiza estrutura html e css da HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import { DEVICES } from "@/dataset/devices";
 import { DeviceCardComponent } from "@/components/DeviceCard";
 import { FaqSectionComponent } from "@/components/FaqSection";
 import { SubscriptionSectionComponent } from "@/components/SubscriptionSection";
+import { BannerComponent } from "@/components/Banner";
 
 export default async function HomePage() {
   const movieGenresData = await fetchMovieGenres();
@@ -21,32 +22,34 @@ export default async function HomePage() {
 
   return (
     <main className={styles["main__home"]}>
-      <section className={styles["sec-background-image__home"]}>
-        <div className={styles["container__home"]}>
-          <div className={styles["subcontainer__home"]}>
+      <section className={styles["image-section__home"]}>
+        <div className={styles["image-container__home"]}>
+          <div className={styles["background-image-container__home"]}>
             <Image
               src="/background.png"
               alt="Background Image from Application"
               width={1920}
               height={860}
-              className={styles["background-image__home"]}
+              className={styles["image__home"]}
             />
           </div>
-          <div className={styles["subcontainer__home"]}>
+          <div className={styles["text-image-container__home"]}>
             <div className={styles["text-container__home"]}>
               <h1 className={styles["heading__home"]}>
-                The best plataform to find movies and TV series
+                The Best Streaming Experience
               </h1>
-              <p className={styles["p__home"]}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
-                corrupti perspiciatis recusandae deleniti quaerat aperiam ipsam
-                cupiditate, velit rerum voluptate! Ducimus veritatis aspernatur
-                ipsa magni nam blanditiis nobis odit asperiores.
+              <p className={styles["text__home"]}>
+                omni+ is the best streaming experience for watching your
+                favorite movies and shows on demand, anytime, anywhere. With
+                omni+, you can enjoy a wide variety of content, including the
+                latest blockbusters, classic movies, popular TV shows, and more.
+                You can also create your own watchlists, so you can easily find
+                the content you want to watch.
               </p>
-              <button className={styles["button__home"]}>
-                Start Watching Now
-              </button>
             </div>
+            <button className={styles["button__home"]}>
+              Start Watching Now
+            </button>
           </div>
         </div>
       </section>
@@ -65,7 +68,6 @@ export default async function HomePage() {
           />
         </div>
       </section>
-
       <section className={styles["section-devices__home"]}>
         <div className={styles["text-devices-container__home"]}>
           <h2 className={styles["h2-devices__home"]}>
@@ -84,9 +86,9 @@ export default async function HomePage() {
           ))}
         </div>
       </section>
-
       <FaqSectionComponent />
       <SubscriptionSectionComponent />
+      <BannerComponent />
     </main>
   );
 }
