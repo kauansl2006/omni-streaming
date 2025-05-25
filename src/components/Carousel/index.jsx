2397446b8f408f62c6a0223cd59b040f38cd3fd1
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-25 19:25:35 -0300

    feat: cria componente Carousel

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import styles from "./carousel.module.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, Pagination } from "swiper/modules";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { BackdropComponent } from "../Backdrop";
+import { Suspense } from "react";
+
+export const CarouselComponent = ({ items }) => {
+  return (
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={true}
+        pagination={{ clickable: true }}
+        loop={true}
+      >
+        {
+          items.map((item) => (
+            <SwiperSlide key={item.id}>
+              <Suspense fallback={<div className={styles["loading__carousel"]}>Loading backdrops...</div>} >
+                <BackdropComponent  item={item} />
+              </Suspense>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+  )
+}
