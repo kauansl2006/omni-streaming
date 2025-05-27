c19d8c3d4da4d020e27239e287c22e513a5ce726
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-05-26 21:27:56 -0300

    refactor: atualiza nomenclatura de estilos css dos componentes Header e Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,157 +5,157 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className={styles["footer__footer"]}>
-      <div className={styles["container__footer"]}>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Home</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="/#categories-section" className={styles["item__footer"]}>
+      <div className={styles["footer__container"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Home</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/#categories-section" className={styles["footer__item"]}>
                 Categories
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/#devices-section" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/#devices-section" className={styles["footer__item"]}>
                 Devices
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/#subscription-section" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/#subscription-section" className={styles["footer__item"]}>
                 Pricing
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/#faq-section" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/#faq-section" className={styles["footer__item"]}>
                 FAQ
               </Link>
             </li>
           </ul>
         </div>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Movies</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#movies-gernres" className={styles["item__footer"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Movies</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#movies-gernres" className={styles["footer__item"]}>
                 Genres
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#discover-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#discover-movies" className={styles["footer__item"]}>
                 Discover
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#popular-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#popular-movies" className={styles["footer__item"]}>
                 Popular
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#top-rated-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#top-rated-movies" className={styles["footer__item"]}>
                 Top Rated
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#upcoming-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#upcoming-movies" className={styles["footer__item"]}>
                 Upcoming
               </Link>
             </li>
           </ul>
         </div>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Shows</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#shows-gernres" className={styles["item__footer"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Shows</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#shows-gernres" className={styles["footer__item"]}>
                 Genres
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#discover-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#discover-movies" className={styles["footer__item"]}>
                 Discover
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#airing-today-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#airing-today-movies" className={styles["footer__item"]}>
                 Airing Today
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#on-the-air" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#on-the-air" className={styles["footer__item"]}>
               On The Air 
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#popular-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#popular-movies" className={styles["footer__item"]}>
                 Popular
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/movies-and-shows#top-rated-movies" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/movies-and-shows#top-rated-movies" className={styles["footer__item"]}>
                 Top Rated
               </Link>
             </li>
           </ul>
         </div>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Suport</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="/suport#suport-section" className={styles["item__footer"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Suport</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/suport#suport-section" className={styles["footer__item"]}>
                 Contact Us
               </Link>
             </li>
           </ul>
         </div>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Subscription</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="/subscription#plans-section" className={styles["item__footer"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Subscription</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/subscription#plans-section" className={styles["footer__item"]}>
                 Plans
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="/subscription#comparasion-section" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="/subscription#comparasion-section" className={styles["footer__item"]}>
                 Features
               </Link>
             </li>
           </ul>
         </div>
-        <div className={styles["subcontainer__footer"]}>
-          <h6 className={styles["heading__footer"]}>Connect With Us</h6>
-          <ul className={styles["ul__footer"]}>
-            <li className={styles["li__footer"]}>
-              <Link href="#" className={styles["item__footer"]}>
+        <div className={styles["footer__subcontainer"]}>
+          <h6 className={styles["footer__heading"]}>Connect With Us</h6>
+          <ul className={styles["footer__ul"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="#" className={styles["footer__item"]}>
                 item 1
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="#" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="#" className={styles["footer__item"]}>
                 item 2
               </Link>
             </li>
-            <li className={styles["li__footer"]}>
-              <Link href="#" className={styles["item__footer"]}>
+            <li className={styles["footer__li"]}>
+              <Link href="#" className={styles["footer__item"]}>
                 item 3
               </Link>
             </li>
           </ul>
         </div>
       </div>
-      <div className={styles["container__footer"]}>
-        <p className={styles["text__footer"]}>
+      <div className={styles["footer__container"]}>
+        <p className={styles["footer__text"]}>
           @2025 omni-streaming, All Rights Reserved
         </p>
-        <div className={styles["terms-and-privacy__footer"]}>
-          <Link href="#" className={styles["link__footer"]}>
+        <div className={styles["footer__terms-and-privacy"]}>
+          <Link href="#" className={styles["footer__link"]}>
             Terms of Use
           </Link>
           |
-          <Link href="#" className={styles["link__footer"]}>
+          <Link href="#" className={styles["footer__link"]}>
             Privacy Policy
           </Link>
           |
-          <Link href="#" className={styles["link__footer"]}>
+          <Link href="#" className={styles["footer__link"]}>
             Cookie Policy
           </Link>
         </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,41 +6,41 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <header className={styles["header__header"]}>
-      <div className={styles["container__header"]}>
-        <div className={styles["logo-container__header"]}>
+      <div className={styles["header__container"]}>
+        <div className={styles["header__logo-container"]}>
           <Image
             src="/logo.svg"
             alt="Aplication Logo"
             width={500}
             height={500}
-            className={styles["logo__header"]}
+            className={styles["header__logo"]}
           />
         </div>
-        <nav className={styles["nav__header"]}>
-          <ul className={styles["ul__header"]}>
-            <li className={styles["li__header"]}>
-              <Link href="/" className={styles["item__header"]}>
+        <nav className={styles["header__nav"]}>
+          <ul className={styles["header__ul"]}>
+            <li className={styles["header__li"]}>
+              <Link href="/" className={styles["header__item"]}>
                 Home
               </Link>
             </li>
-            <li className={styles["li__header"]}>
-              <Link href="/movies-and-shows" className={styles["item__header"]}>
+            <li className={styles["header__li"]}>
+              <Link href="/movies-and-shows" className={styles["header__item"]}>
                 Movies & Shows
               </Link>
             </li>
-            <li className={styles["li__header"]}>
-              <Link href="/suport" className={styles["item__header"]}>
+            <li className={styles["header__li"]}>
+              <Link href="/suport" className={styles["header__item"]}>
                 Suport
               </Link>
             </li>
-            <li className={styles["li__header"]}>
-              <Link href="/subscription" className={styles["item__header"]}>
+            <li className={styles["header__li"]}>
+              <Link href="/subscription" className={styles["header__item"]}>
                 Subscription
               </Link>
             </li>
           </ul>
         </nav>
-        <div className={styles["buttons-container__header"]}>
+        <div className={styles["header__buttons-container"]}>
           <button>Sign In</button>
         </div>
       </div>
