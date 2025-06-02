3271c5c2e59bf72ceedc9a540b4696c8760f2266
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-06-02 13:38:36 -0300

    refactor: renderiza temporadas e episódios

--- src/components/DetailsList/index.jsx
+++ src/components/DetailsList/index.jsx
@@ -4,6 +4,7 @@ import {
   fetchDetailsById,
   fetchCreditsById,
   fetchReviewsById,
+  fetchTvSeasonDetails,
  } from "@/lib/data";
  
 import { LANGUAGES } from "@/constants/languages";
@@ -11,6 +12,7 @@ import { LANGUAGES } from "@/constants/languages";
 import { ReviewsList } from "@/components/ReviewsList";
 import { CastList } from "@/components/CastList";
 import { Item } from "./Item";
+import { Season } from "./Season";
 
 export const DetailsList = async ({ id, type }) => {
   const data = await fetchDetailsById(Number(id), type);
@@ -20,9 +22,29 @@ export const DetailsList = async ({ id, type }) => {
   );
   const reviews = await fetchReviewsById(Number(id), type);
 
+  const seasonWithEpisodes = await Promise.all(
+    data.seasons?.map(async (season) => {
+      const { episodes } = await fetchTvSeasonDetails(data.id, season.season_number);
+      return {
+        ...season,
+        episodes
+      }
+    }) || []
+  );
+
   return (
     <div className={styles["details-list__container"]}>
       <div className={styles["details-list__subcontainer"]}>
+        <div className={styles["details-list__card-container"]}>
+          <h5 className={styles["details-list__heading"]}>Seasons and Episodes</h5>
+          {
+            seasonWithEpisodes.map((season) => (
+              <Season key={season.id} season={season} />
+            ))
+          }
+        </div>
+
+
         <div className={styles["details-list__card-container"]}>
           <h5 className={styles["details-list__heading"]}>Description</h5>
           <p className={styles["details-list__text"]}>{data.overview}</p>
@@ -45,7 +67,7 @@ export const DetailsList = async ({ id, type }) => {
       </div>
 
       <div className={styles["details-list__subcontainer"]}>
-        <Item heading={"Released Year"} releaseDate={data.release_date.split("-")[0]} />
+        <Item heading={"Released Year"} releaseDate={type === "movie" ? data.release_date.split("-")[0] : data.first_air_date.split("-")[0]} />
         <Item heading={"Genres"} genres={data.genres} />
         <Item heading={"Rating"} rating={true} />
         <Item heading={"Available Languages"} languages={LANGUAGES} />
